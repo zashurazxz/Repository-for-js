32b0c5d35a81c59fc299fdb2352d64ed441d4afe
Xavier <43177673+zashurazxz@users.noreply.github.com>
2018-10-18 01:27:08 -0600

    assignment

--- Day 7/assignment/script.js
+++ Day 7/assignment/script.js
@@ -1,57 +1,33 @@
-// const form = document.querySelector('form');
-// const ul = document.querySelector('ul');
-// const button = document.querySelector('button');
-// const input = document.getElementById('item');
-// let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
-
-// localStorage.setItem('items', JSON.stringify(itemsArray));
-// const data = JSON.parse(localStorage.getItem('items'));
-
-// const liMaker = (text) => {
-//   const li = document.createElement('li');
-//   li.textContent = text;
-//   ul.appendChild(li);
-// }
-
-// form.addEventListener('submit', function (e) {
-//   e.preventDefault();
-
-//   itemsArray.push(input.value);
-//   localStorage.setItem('items', JSON.stringify(itemsArray));
-//   liMaker(input.value);
-//   input.value = "";
-// });
-
-// data.forEach(item => {
-//   liMaker(item);
-// });
-
-// button.addEventListener('click', function () {
-//   localStorage.clear();
-//   while (ul.firstChild) {
-//     ul.removeChild(ul.firstChild);
-//   }
-// });
-
-// fetch("https://api.lyrics.ovh/v1/artist/title")
-// .then(function(response){
-//     return response.json();
-// })
-// .then(function(data){
-//     console.log(data)
-// })
-
-var request = new XMLHttpRequest();
-
-request.open('GET', 'https://api.lyrics.ovh/v1/Coldplay/Adventure of a Lifetime');
-
-request.onreadystatechange = function () {
-  if (this.readyState === 4) {
-    console.log('Status:', this.status);
-    console.log('Headers:', this.getAllResponseHeaders());
-    console.log('Body:', this.responseText);
-  }
-};
-
-request.send();
-
+let funcButton=document.getElementById("lyricsGrabber");
+lyricsGrabber.addEventListener("click", ()=>{
+let artist=document.lyricForm.Name.value;
+let song=document.lyricForm.Song.value;
+let url="https://api.lyrics.ovh/v1/" + artist +"/" + song
+fetch(url, {
+method:"GET",
+headers:{
+  'Content-type':'application/json'
+}
+})         
+.then((results) => {
+return results.json();
+})
+.then((resultspt2) => {
+if(resultspt2.error!=null){
+  let divHold=document.getElementById("container");
+  divHold.innerHTML="";
+  divHold.innerHTML="There was an Error, please try again";
+}
+else{
+  let songArray=((resultspt2.lyrics).split("\n"));
+  let divHolder=document.getElementById("container");
+  divHolder.innerHTML="";
+  for(let i of songArray){
+  let Newp=document.createElement("p");
+    Newp.innerHTML=i;
+    Newp.classList.add("lyricP");
+    container.appendChild(Newp);
+} 
+}
+})
+});
